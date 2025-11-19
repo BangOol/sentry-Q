7a3f0c025bdb3c6b3f2f8aa3ae35b41846e9df9b
BangOol <yjg05511@gmail.com>
2025-11-19 21:08:28 +0900

    RDBMS 테이블과 매칭되지 않기 때문에 Entity 클래스가 필요없으므로 삭제 진행.

--- src/main/java/com/sentryq/thunder/repository/userRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sentryq.thunder.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.sentryq.thunder.domain.User;
-
-public interface userRepository extends JpaRepository<User, Long> {
-
-}
