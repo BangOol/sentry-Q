7d48cd526d74ca54d0035abc509fae2eba230db9
BangOol <yjg05511@gmail.com>
2025-11-24 00:06:26 +0900

    RDMBS에 직접적으로 접근하기 않기 때문에 삭제

--- src/main/java/com/sentryq/thunder/repository/EventRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sentryq.thunder.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.sentryq.thunder.domain.Event;
-
-public interface EventRepository extends JpaRepository<Event, Long> {
-
-}
